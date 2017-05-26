82ff8f2941ec529311757ff80d22fe2d8a23a26f
Tomas Avé <cod3hulk@gmail.com>
2017-05-26 09:25:02 +0200

    Handle missing values

--- Part 1 - Data Preprocessing/main.py
+++ Part 1 - Data Preprocessing/main.py
@@ -6,3 +6,10 @@ import ipdb; ipdb.set_trace()
 dataset = pd.read_csv("Data.csv")
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+# Taking care of missing values
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(x[:, 1:3])
+import ipdb; ipdb.set_trace()
+x[:, 1:3] = imputer.transform(x[:, 1:3])
