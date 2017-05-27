01e46b63c0ca5f0bbf8a3c062e6e3c661e97a0ba
Tomas Avé <cod3hulk@gmail.com>
2017-05-27 23:25:05 +0200

    Add feature scaling

--- Part 1 - Data Preprocessing/main.py
+++ Part 1 - Data Preprocessing/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv("Data.csv")
+
+# Split into dependent and independent variables
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
@@ -29,4 +31,9 @@ from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-print 'Done.'
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
