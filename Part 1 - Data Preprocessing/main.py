61ae12e5a850bf1d82838e38a98d536245bb1898
Tomas Avé <cod3hulk@gmail.com>
2017-05-27 22:57:55 +0200

    Split dataset into training and test set

--- Part 1 - Data Preprocessing/main.py
+++ Part 1 - Data Preprocessing/main.py
@@ -20,3 +20,13 @@ x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
 
 onehoteencoder = OneHotEncoder(categorical_features=[0])
 x = onehoteencoder.fit_transform(x).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Splitting the dataset into the training and test set
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+print 'Done.'
